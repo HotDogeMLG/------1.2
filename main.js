6648627f74aecdb3af1739705410aa9cafa3a113
Roman Masik <darth_vader_66@mail.ru>
2023-08-08 13:44:31 +0300

    Implemented Laptop Advantages Block

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
 menuButtonFunction = () => {
     document.querySelector("header").classList.toggle("open")
 }
+
+console.log('1')
+advArrowFunction = () => {
+    const LaptopAdvItems = document.querySelectorAll(".laptop-adv__item")
+    LaptopAdvItems.forEach((element) => {
+        element.classList.toggle("laptop-adv__item_active")
+    })
+    document.querySelector(".adv__arrow").classList.toggle("adv__arrow_active")
+    document.querySelector(".adv .price").classList.toggle("price_active")
+}
